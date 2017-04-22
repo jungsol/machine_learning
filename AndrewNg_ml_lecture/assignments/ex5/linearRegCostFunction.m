5b7a6d2ac18ae9962fa917972854a5ebb6974489
JungSol <solbread0@gmail.com>
2017-04-22 23:58:47 +0900

    solve regularized linear reg cost function

--- AndrewNg_ml_lecture/assignments/ex5/linearRegCostFunction.m
+++ AndrewNg_ml_lecture/assignments/ex5/linearRegCostFunction.m
@@ -21,7 +21,7 @@ grad = zeros(size(theta));
 
 hx = X * theta;
 
-J = 1/(2*m)*sum((hx-y).^2) + lambda * sum(theta(2:end).^2);
+J = 1/(2*m)*(sum((hx-y).^2) + lambda * sum(theta(2:end).^2));
 
 grad = 1/m*(X'*(hx-y));
 grad(2:end) = grad(2:end) + (lambda / m * theta(2:end));
